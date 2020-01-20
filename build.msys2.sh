6fe972cbad253bd340ebb4157d92ea0ef9da1ea8
Felix von Poblotzki <xilaew@gmail.com>
2020-01-20 02:52:40 +0100

    dlls are collected automatically with copydlldeps.sh (copied from mxe project)

--- build.msys2.sh
+++ build.msys2.sh
@@ -6,8 +6,20 @@ rm -rf build
 # meson needs soffice.exe to be on the PATH in order to find it
 export PATH=$PATH:/c/Program\ Files/LibreOffice/program
 # the meson application comming with MSYS does not work. Use the one installed on Windows instead.
-export PATH=/c/Program\ Files/meson:$PATH:/c/Program\ Files/LibreOffice/program
+export PATH=/c/Program\ Files/meson:$PATH
 meson build
 cd build
 ninja
 DESTDIR="$PWD/install" ninja install
+
+# Copy all DLLs into installation directory
+pushd install/bin
+../../../copydlldeps.sh -d . -F . -R /mingw64 -s /usr/bin -s /bin
+popd
+
+# add gdk pixbuff loaders (without them the app crashes)
+mkdir -p install/lib/gdk-pixbuf-2.0/2.10.0/loaders
+cp /mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache install/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+cp /mingw64/lib/gdk-pixbuf-2.0/2.10.0/loaders/*.dll install/lib/gdk-pixbuf-2.0/2.10.0/loaders/
+
+cp -R ../share ./install/
